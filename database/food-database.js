bb6d40eb72f4389aa21fcbf6e0d13800e583dc5f
henNet <dmf.n123@gmail.com>
2024-06-02 00:37:44 -0300

    Modificado a url de conecção com o banco postgres na Vercel

--- database/food-database.js
+++ database/food-database.js
@@ -1,6 +1,6 @@
-import pg from 'pg'
+import pg from "pg";
 import env from "dotenv";
-const { Pool } = pg
+const { Pool } = pg;
 
 /* carrega o dotenv, para que a variável 
    com a string de conexão vá parar na memória do programa. */
@@ -9,12 +9,16 @@ env.config();
 // const connectionString = 'postgresql://postgres:123@localhost:5432/foods'
 
 /* ==> Conexão com a Base de Dados: */
-const pool = new Pool({
-    connectionString: process.env.CONNECTION_STRING,
-  })
+// const pool = new Pool({
+//   connectionString: process.env.CONNECTION_STRING,
+// });
 
-pool.on('connect', () => {
-  console.log('Base de Dados conectado com sucesso!');
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+})
+
+pool.on("connect", () => {
+  console.log("Base de Dados conectado com sucesso!");
 });
 
 export default pool;
